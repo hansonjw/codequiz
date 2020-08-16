a0ffe8056debf06164e17b721ab5ad571d0af4fe
hansonjw <hansonjw@gmail.com>
2020-08-16 16:11:06 -0500

    Add CSS, debug multiple elements including timer and button handling.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # codequiz
-UCB Bootcamp assignment Week 3
+UCB Bootcamp assignment Week 4
+Submitted by:       Justin Hanson
+Submission date:    August 16, 2020
+
+This application is a Quiz style web application that utilizes a timer, an array of questions and answers, event handling, dynamic HTML DOM element creation, among other things.
+
+The primary objective of the user is to answer as many questions as possible in the available time.  Is a question answered incorrectly there is a 5 second time penalty.  At the end of the game the user has can input thier name and the application will log thier name and score to local storage.
 
 
-Acceptance Criteria:
+Below is the Acceptance Criteria as required by the class:
 
 GIVEN I am taking a code quiz
 WHEN I click the start button
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,173 @@
 // Code Quiz JS
 
-var questionAnswerEl = document.querySelector("#q-and-a");
-var timerEl = document.querySelector("#timerDisplay");
+// Links to HTML elements "parents"
+var gamePlayEl = document.querySelector("#gamePlay");
 var startSession = document.querySelector("#page-content");
 var resultsDisplay = document.querySelector("#results");
+var playerName = '';
 
+// Set initial game variables
 var score = 0;
 var roundArrayCnt = 0;
 var timePenalty = 5;
 var counter = 45;
 
-// This is a list of questions, answers, and correct answer for scrore keeping
+// list of questions, answers, and correct answer for scrore keeping
 var questions = [
   {q: "what is 1...?", a1: "this one!", a2: "not this one", a3: "not this one", a4: "not this one", correct: 'ans1'},
   {q: "what is 2...?", a1: "not this one", a2: "this one!", a3: "not this one", a4: "not this one", correct: 'ans2'},
   {q: "what is 3...?", a1: "not this one", a2: "not this one", a3: "this one!", a4: "not this one", correct: 'ans3'},
   {q: "what is 4...?", a1: "not this one", a2: "not this one", a3: "not this one", a4: "this one!", correct: 'ans4'},
-  {q: "what is 5...?", a1: "this one!", a2: "not this one", a3: "not this one", a4: "not this one", correct: 'ans1'},
-];
+  {q: "what is 5...?", a1: "this one!", a2: "not this one", a3: "not this one", a4: "not this one", correct: 'ans1'},];
 
-// Main timer function...called at the start of a new game
+// Create start button, show on initial load of webpage, remove later
+var startButton = document.createElement('button');
+startButton.textContent = "Start New Game";
+startButton.setAttribute("id", "start");
+gamePlayEl.appendChild(startButton);
+startButton.setAttribute("class", "otherButtons");
+
+// Create elements for answer buttons, the display question, and the timer display
+var ansEl1 = document.createElement('button');
+var ansEl2 = document.createElement('button');
+var ansEl3 = document.createElement('button');
+var ansEl4 = document.createElement('button');
+var questionEl = document.createElement('h2');
+var timerEl = document.createElement('h2');
+
+// Create final score and playern name elements
+var playerNameLableEl = document.createElement('label');
+var playerNameEl = document.createElement('input');
+var scoreEl = document.createElement('h2');
+// var scoreEl = document.createElement('h2');
+var logNameScoreEl = document.createElement('button');
+
+// Set button IDs so buttons can be handled, answers can be checked against 'correct' in questions array, etc.
+ansEl1.setAttribute("id", "ans1");
+ansEl2.setAttribute("id", "ans2");
+ansEl3.setAttribute("id", "ans3");
+ansEl4.setAttribute("id", "ans4");
+
+ansEl1.setAttribute("class", "ansbutton");
+ansEl2.setAttribute("class", "ansbutton");
+ansEl3.setAttribute("class", "ansbutton");
+ansEl4.setAttribute("class", "ansbutton");
+
+logNameScoreEl.setAttribute("id", "logScore");
+logNameScoreEl.setAttribute("class", "otherButtons");
+answerIds = ["ans1", "ans2", "ans3", "ans4"]
+
+
+// Main timer function that is called at the start of a new game
 function timer() {
   var timeInterval = setInterval(function() {
+    
     if (counter <= 0) {
       console.log(counter);
       console.log("END OF THE GAME");
+      timerEl.textContent = "Time is up!!";
       clearInterval(timeInterval);
+      setTimeout(endGame, 1000);
     }
     else{
       console.log(counter);
-      document.getElementById("timerDisplay").innerHTML = "seconds remaining: " + counter;
+      timerEl.textContent = "Timer: " + counter + " seconds remaining";
     }
   counter--;
   },1000)
 }
 
 
-// A start game function that resets counters, scores, initiates timer, etc...
+// Start game function that resets counters, scores, initiates timer, etc...
 function startNewGame() {
-  console.log("you pushed: " + event.target.id);
+  gamePlayEl.removeChild(startButton);
   score = 0;
+  counter = 30;
   roundArrayCnt = 0;
-  console.log(score);
-  console.log(roundArrayCnt);
-  displayQuestion(roundArrayCnt);
+  timerEl.textContent = "Ready, set, go!";
   timer();
+  gamePlayEl.appendChild(timerEl);
+  gamePlayEl.appendChild(questionEl);
+  gamePlayEl.appendChild(ansEl1);
+  gamePlayEl.appendChild(ansEl2);
+  gamePlayEl.appendChild(ansEl3);
+  gamePlayEl.appendChild(ansEl4);
+  displayQuestion(roundArrayCnt);
 }
 
-// A function that ends the game and displays the score, etc
+
+// Ends the game, sets timer to 0, clears game play area, calls function to display score
 function endGame(){
-  console.log("End Game function executed");
   counter = 0;
 
   // Reset question buttons
-  document.getElementById("question").innerHTML = null;
-  document.getElementById("ans1").innerHTML = null;
-  document.getElementById("ans2").innerHTML = null;
-  document.getElementById("ans3").innerHTML = null;
-  document.getElementById("ans4").innerHTML = null;
-  
-  // display the score - do this with append child in the results area
-  // display a text entry box for player's name <input>, append child
-  // display an "enter score bustton"
+  while (gamePlayEl.hasChildNodes()){
+    gamePlayEl.removeChild(gamePlayEl.firstChild); 
+  }
+  displayNameScore();
+}
 
-  
 
+function displayNameScore(){
+
+  playerNameLableEl.textContent = "Enter Your Name: ";
+  resultsDisplay.appendChild(playerNameLableEl);
+  console.log(playerNameEl);
+  console.log(typeof playerNameEl);
+  resultsDisplay.appendChild(playerNameEl);
+
+  scoreEl.textContent = "You answered " + score + " question(s) correctly";
+  resultsDisplay.appendChild(scoreEl);
+
+  logNameScoreEl.textContent = "Log Name and Score";
+  resultsDisplay.appendChild(logNameScoreEl);
+}
+
+function logNameScore(){
+  playerName = playerNameEl.value;
   localStorage.setItem("score", score);
   localStorage.setItem("player", playerName);
+  
+  //reset player name for the next game
+  playerName = '';
+  playerNameEl.value = '';
+
+  while (resultsDisplay.hasChildNodes()){
+    resultsDisplay.removeChild(resultsDisplay.firstChild);
+  }
+
+  gamePlayEl.appendChild(startButton);
 }
+
 
 // This function takes in the round count/question number and displays
 // the question and multiple choice answers...
-// This should be done with append child 
 function displayQuestion(qNum) {
-  document.getElementById("question").innerHTML = questions[qNum].q;
-  document.getElementById("ans1").innerHTML = questions[qNum].a1;
-  document.getElementById("ans2").innerHTML = questions[qNum].a2;
-  document.getElementById("ans3").innerHTML = questions[qNum].a3;
-  document.getElementById("ans4").innerHTML = questions[qNum].a4;
+  questionEl.textContent = questions[qNum].q;
+  ansEl1.textContent = questions[qNum].a1;
+  ansEl2.textContent = questions[qNum].a2;
+  ansEl3.textContent = questions[qNum].a3;
+  ansEl4.textContent = questions[qNum].a4;
 }
+
 
 // This function handles answer button clicks and determines
 // if the answer was correct, then updates the score and round accordingly
 function answerButton(buttonId){
-
   // did you push the right answer?
   if(buttonId == questions[roundArrayCnt].correct){
     score++;
   }else{
     counter = counter - timePenalty;
+    var tempCounter = counter + 1;
+    timerEl.textContent = "Timer: " + tempCounter + " seconds remaining";
   }
-
-  // on to the next round
+  // on to the next round, if end execute endGame
   roundArrayCnt++;
   if (roundArrayCnt >= questions.length){
-    endGame();
+    counter = 0;
   }else{
-    displayQuestion(roundArayCnt);
+    displayQuestion(roundArrayCnt);
   }
 }
 
@@ -104,12 +175,22 @@ function answerButton(buttonId){
 // This function is the main handler of button click events
 // calls other functions based on the button ID
 function buttonClicks(event) {
+  event.stopPropogation;
   var buttonId = event.target.id;
+  console.log(buttonId);
   if (buttonId == "start"){
     startNewGame();
-  }else {
+  }
+  else if(buttonId == "logScore"){
+    logNameScore();
+  }
+  else if(answerIds.includes(buttonId)) {
     answerButton(buttonId);
   }
+  else{
+    return;
+  }
 } 
+
 
 startSession.addEventListener("click", buttonClicks);
